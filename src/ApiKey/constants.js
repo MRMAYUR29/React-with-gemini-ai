4a7c6cc422527e6251d1186b58f4cc81abb4d1ac
Chirag Padhyal <55888522+MRMAYUR29@users.noreply.github.com>
2025-12-04 20:11:54 +0530

    pushing all my changes into the repo

---
+++ src/ApiKey/constants.js
@@ -0,0 +1,19 @@
+export const URL ="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDqOXsUpceEojx9rcQmz5LaDXgj_1j4l4k"
+
+// curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" \
+//   -H 'Content-Type: application/json' \
+//   -H 'X-goog-api-key: AIzaSyDqOXsUpceEojx9rcQmz5LaDXgj_1j4l4k' \
+//   -X POST \
+//   -d '{
+//     "contents": [
+//       {
+//         "parts": [
+//           {
+//             "text": "Explain how AI works in a few words"
+//           }
+//         ]
+//       }
+//     ]
+//   }'
+
+  
